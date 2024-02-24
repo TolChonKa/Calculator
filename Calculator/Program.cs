44fed1b25a0b674040e9d3a00f66f830a7e81ea8
TolChonKa <gekysik123@gmail.com>
2024-02-24 11:28:50 +0300

    calc

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -7,8 +7,8 @@ namespace Calculator
 
         public class Operations
         {
-            //static string exception = "Ошибка! Выберете операцию, которая представлена.";
             static double result;
+            static double[] massiv_volue = new double[1];
             public static double OperationsValue(double firstvalue, double secondvalue, string operation)
             {
                 switch (operation)
@@ -28,9 +28,21 @@ namespace Calculator
                     case "^":
                         result = Math.Pow(firstvalue, secondvalue);
                         return result;
-                    default: return 0;
+                    default:
+                        return 0;
+
+                }
+                
+            }
+            public static void Massiv()
+            {
+                for (int i = 0; i < massiv_volue.Length; i++)
+                {
+                    massiv_volue[i] = result;
+
                 }
             }
+
             public static double OperationEngener(double number, string operation)
             {
                 switch (operation)
@@ -121,104 +133,54 @@ namespace Calculator
         static void Main(string[] args)
         {
             Operations operations = new Operations();
-            string operation, time_variable;
-            double result, firstvalue, secondvalue, number, result_number;
-
-
-            Console.Write("Выберете какую операцию хотите выполнить (+, -, *, /, sin, cos, tg, arctg, log, ln, !, 1/x , exp, ^ : ");
-            operation = Console.ReadLine();
-            //if (time_variable == "+" || time_variable == "-" || time_variable == "*" || time_variable == "/" || time_variable == "^" || time_variable == "sin" || time_variable == "cos" || time_variable == "tg" || time_variable == "arctg" || time_variable == "ln" || time_variable == "!" || time_variable == "log" || time_variable == "1/x" || time_variable == "exp")
-            //{
-            //    operation = time_variable;
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Ошибка! Введите операцию из списка.");
-            //}
-
-            if (operation == "+" || operation == "-" || operation == "*" || operation == "/" || operation == "^")
+            while (true)
             {
-                Console.Write("\nВведите первое число: ");
-                string ReadVolue = Console.ReadLine();
-                Check.CheckVolue(ReadVolue);
-                firstvalue = Check._firstvalue;
+                double[] massiv = new double[1];
+                string operation;
+                double result, firstvalue, secondvalue, number, result_number;
 
-                Console.Write("\nВведите второе число: ");
-                string ReadVolue2 = Console.ReadLine();
-                Check.CheckVolue2(ReadVolue2);
-                secondvalue = Check._secondvalue;
 
-                result = Operations.OperationsValue(firstvalue, secondvalue, operation);
-                Console.WriteLine($"\nРезультат вычислений: {result}");
+                Console.Write("Выберете какую операцию хотите выполнить (+, -, *, /, sin, cos, tg, arctg, log, ln, !, 1/x , exp, ^ : ");
+                operation = Console.ReadLine();
+
+
+                if (operation == "+" || operation == "-" || operation == "*" || operation == "/" || operation == "^")
+                {
+                    Console.Write("\nВведите первое число: ");
+                    string ReadVolue = Console.ReadLine();
+                    Check.CheckVolue(ReadVolue);
+                    firstvalue = Check._firstvalue;
+
+                    Console.Write("\nВведите второе число: ");
+                    string ReadVolue2 = Console.ReadLine();
+                    Check.CheckVolue2(ReadVolue2);
+                    secondvalue = Check._secondvalue;
+
+                    result = Operations.OperationsValue(firstvalue, secondvalue, operation);
+
+                    Operations.Massiv();
+                    Console.WriteLine($"\nРезультат вычислений: {result}");
+                }
+
+                else
+                {
+                    Console.Write("\nВведите число: ");
+                    string ReadNumber = Console.ReadLine();
+
+                    Check.CheckNumber(ReadNumber);
+                    number = Check.number;
+
+                    result_number = Operations.OperationEngener(number, operation);
+                    Console.WriteLine($"\nРезультат вычислений: {result_number}");
+                }
+
+
+
+
+
+               
+                Console.ReadLine();
             }
-
-            else
-            {
-                Console.Write("\nВведите число: ");
-                string ReadNumber = Console.ReadLine();
-
-                Check.CheckNumber(ReadNumber);
-                number = Check.number;
-
-                result_number = Operations.OperationEngener(number, operation);
-                Console.WriteLine($"\nРезультат вычислений: {result_number}");
-            }
-
-
-
-
-
-
-
-
-
-
-
-            //case "cos":
-            //    result = firstvalue + secondvalue;
-            //    return result;
-            //case "tg":
-            //    result = firstvalue - secondvalue;
-            //    return result;
-            //case "arctg":
-            //    result = firstvalue * secondvalue;
-            //    return result;
-            //case "log":
-            //    result = firstvalue / secondvalue;
-            //    return result;
-            //case "ln":
-            //    result = firstvalue + secondvalue;
-            //    return result;
-            //case "!":
-            //    result = firstvalue - secondvalue;
-            //    return result;
-            //case "1/x":
-            //    result = firstvalue * secondvalue;
-            //    return result;
-            //case "exp":
-            //    result = firstvalue / secondvalue;
-            //    return result;
-
-            //// 1 число
-            //Console.Write("Введите первое число: ");
-            //Check.check_one_volue = Console.ReadLine();
-            //firstvalue = Check._firstvalue;
-
-
-            //// 2 число
-            //Console.Write("Введите второе число: ");
-            //Check.check_two_volue = Console.ReadLine();
-            //secondvalue = Check._secondvalue;
-
-
-
-
-            //Console.Write("Введите операцию (+, -, *, /): ");
-            //operation = Console.ReadLine();
-
-            //var resultoperation = operations.Simple_Operations(_firstvalue, _secondvalue, operation);
-            //Console.WriteLine(resultoperation);
-            Console.ReadLine();
         }
     }
 }
